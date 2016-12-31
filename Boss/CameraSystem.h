3c7f28fe5a8e7f98453e6f22a1f3fec5355d1a92
Titas <Titas@DESKTOP-8AU0SR3>
2016-12-31 19:27:26 +0400

    minor change to structure of CameraSystem

--- Boss/CameraSystem.h
+++ Boss/CameraSystem.h
@@ -18,62 +18,59 @@ struct CameraSystem : System<Requires<PlayerComponent, PositionComponent, RectCo
 	Camera camera;
 	sf::Event event;
 	
-	void unlock_camera() {
-		
-		while (WINDOW.pollEvent(event))
-		{
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::C) {
-				camera.locked = !camera.locked;
-			}
-		}
-
-	}
-
+	void unlock_camera();
+	void update(double deltaTime);
 	
-	
-
-
-	void process(Entity& e, double deltaTime)
-	{
-		
-		sf::View view = camera.view;
-		RectComponent& rectComponent = e.getComponent<RectComponent>();
-		PositionComponent& positionComp = e.getComponent<PositionComponent>();
-
-		unlock_camera();
-
-		if (camera.locked) {
-			camera.position = rectComponent.center;
-			camera.view.setCenter(rectComponent.center.x, rectComponent.center.y);
-		}
-		else {
-			camera.moveWithKeys(deltaTime);
-		}
-			WINDOW.setView(camera.view);
-
-	}
-
-	void update(double deltaTime)
-	{
-		auto entities = getEntities();
-
-
-		for (auto i : entities)
-		{
-			process(i, deltaTime);
-		}
-
-	}
-
-
-	void render(sf::RectangleShape rect)
-	{
-	}
+private:
+	void process(Entity& e, double deltaTime); void process(Entity& e, double deltaTime);
+	void render(sf::RectangleShape rect) {};
 
 };
 
 
+void CameraSystem::update(double deltaTime)
+{
+	auto entities = getEntities();
 
+
+	for (auto i : entities)
+	{
+		process(i, deltaTime);
+	}
+
+}
+
+void CameraSystem::process(Entity& e, double deltaTime)
+{
+
+	sf::View view = camera.view;
+	RectComponent& rectComponent = e.getComponent<RectComponent>();
+	PositionComponent& positionComp = e.getComponent<PositionComponent>();
+
+	unlock_camera();
+
+	if (camera.locked) {
+		camera.position = rectComponent.center;
+		camera.view.setCenter(rectComponent.center.x, rectComponent.center.y);
+	}
+	else {
+		camera.moveWithKeys(deltaTime);
+	}
+	WINDOW.setView(camera.view);
+
+}
+
+
+void CameraSystem::unlock_camera() {
+
+	while (WINDOW.pollEvent(event))
+	{
+		if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::C) {
+			camera.locked = !camera.locked;
+		}
+	}
+
+}
 
 
 #endif // CAMERA_SYSTEM
