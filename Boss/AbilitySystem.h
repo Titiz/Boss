91862a7556ed8bcdb01ff32a05d261e24b24da85
Titas <Titas@DESKTOP-8AU0SR3>
2017-02-17 20:43:58 +0400

    Ability System start

---
+++ Boss/AbilitySystem.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Components.h>
+#include <Constants.h>
+
+#ifndef ABILITY_SYSTEM
+#define ABILITY_SYSTEM
+
+struct AbilitySystem : System<Requires<PlayerComponent, PositionComponent, RectComponent>> {
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif // ABILITY_SYSTEM
