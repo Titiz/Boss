d527c3ef758c2f928ef6305341b334d9d1473f00
Titas <Titas@DESKTOP-8AU0SR3>
2017-02-18 18:42:18 +0400

    Change to the way the dash dissipates

--- include/VelocityVector.h
+++ include/VelocityVector.h
@@ -7,12 +7,18 @@ public:
 	int duration = 1;
 	int time_existed = 0;
 	bool done = false;
+	float difference_x;
+	float difference_y;
 
-	VelocityVector(float x, float y, bool dissipates, int duration) : x(x), y(y), dissipates(dissipates), duration(duration) {};
+	VelocityVector(float x, float y, bool dissipates, int duration) : x(x), y(y), dissipates(dissipates), duration(duration) {
+		difference_x = x / duration;
+		difference_y = y / duration;
+	};
 
 	void dissipate() {
 		if (dissipates) {
-			x -= x / duration;
+			x -= difference_x;
+			y -= difference_y;
 		}
 		time_existed += 1;
 		if (time_existed == duration){
